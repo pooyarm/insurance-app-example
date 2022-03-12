59054cb850c13ecc98b286be15e485ef0ee7dac5
Pooya Rostam <pooya.rostam@eventinc.de>
2022-03-12 14:07:44 +0100

    feat: first name and last name inputs

--- src/buyflow/NameStep.tsx
+++ src/buyflow/NameStep.tsx
@@ -1,14 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 interface NameStepProps {
   cb: (field: string, value: number) => void
 }
 
 const NameStep: React.FC<NameStepProps> = (props) => {
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+
   return (
-    <div>
-      Dummy Name Step
-    </div>
+    <>
+      <div>
+        <label>First name</label>
+        <input
+          type="text"
+          onChange={(event) => setFirstName(event.target.value)}
+          value={firstName}
+        />
+      </div>
+
+      <div>
+        <label>Last name</label>
+        <input
+          type="text"
+          onChange={(event) => setLastName(event.target.value)}
+          value={lastName}
+        />
+      </div>
+    </>
   )
 }
 
